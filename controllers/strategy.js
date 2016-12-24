ca23b8e95ff01a57f6462935ae6e4823826ca763
Felix Mendelssohn <mendelssohn@protonmail.com>
2016-12-24 12:18:30 +0200

    added passport and mysql

---
+++ controllers/strategy.js
@@ -0,0 +1,30 @@
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
+var dbconnect = require("../lib/dbconnect");
+
+function validPassword(passwordClear, PasswordHash) {
+
+}
+
+passport.use(new localStrategy(
+    function(username, password, done) {
+        dbconnect.query("select username from user where username = ?", [username],
+            function(err, results, fields) {
+                if(err) {
+                    console.error(err);
+                    return done(err);
+                }
+
+                if(!results[0].username) {
+                    return done(null, false, { message: "Incorrect username or password!" });
+                }
+
+                if(!validPassword(password, rows[0].password)) {
+                    return done(null, false, { message: "Incorrect username or password!" });
+                }
+
+                return done(null, results[0]);
+            }
+        );
+    }
+));
