9618bd1b70c0d52b9188dfd774930048bde6b2c6
Felix Mendelssohn <mendelssohn@protonmail.com>
2016-12-24 18:57:27 +0200

    added view engine

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 app = express();
 
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
