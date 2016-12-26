7ad20dc85aae328956c6bb92e10830c91a30b847
Amraboelkher <amrkerr@hotmail.com>
2016-12-27 00:06:39 +0200

    adding mailing features

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,12 +1,27 @@
 
 var User = require('../models/user');
 var userController = require('./userController');
+var emailStructure = {
+  from: String,
+  to: String,
+  subject: String,
+  message: String,
+  starred: Boolean
+};
 
 module.exports = {
     addMail : function(emailAddress , mail , callback ){
+        console.log(" ---> " + emailAddress);
         userController.retrieve(emailAddress , function(user){
             if(!user)   return callback(new Error("There is no such user  "));
-            user.inbox.push(mail);
+            var _message = {
+                from: mail.from[0].address,
+                to: mail.to[0].address,
+                subject: mail.subject,
+                message: mail.text,
+                starred: false
+            };
+            user.inbox.push(_message);
             user.save();
             return callback(null);
         });
