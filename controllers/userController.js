7045b11dac4855c44d3050b6616242b847450aa8
Felix Mendelssohn <mendelssohn@protonmail.com>
2016-12-26 16:44:08 +0200

    updated smtp server

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,13 +15,21 @@ module.exports = {
     //returns the user object if found and null if not found
     retrieve: function(email, callback) {
         var result = null;
-        var query = User.where({ email: email });
+        console.log("RETRIVE: " + email);
+        query = User.where({ email: email });
         query.findOne(function(err, user) {
             //TO-DO: change in production
             if(err) console.error(err);
             if(user) result = user;
 
             return callback(result);
+        });
+    },
+
+    retrieveById: function(id, callback) {
+        User.findById(id, function (err, user) {
+            if(err) console.error(err);
+            return callback(user);
         });
     },
 
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mailer = require('../lib/mailer');
+var userController = require('../controllers/userController');
 
 router.get('/:id', function(req, res, next) {
     res.render('home', { id: req.params.id });
+});
+
+router.post('/send/:id', function(req, res, next) {
+    userController.retrieveById(req.params.id, function(user) {
+        if(!user) res.json({ success: false, message: "Username or Password Incorrect!" });
+        mailer.send(user.email, user.password, {
+            from: req.body.from,
+            to: req.body.to,
+            subject: req.body.subject,
+            text: req.body.text
+        }, function(err) {
+            if(err) res.json({ success: false, message: err });
+            else res.json({ success: true });
+        });
+    });
 });
 
 module.exports = router;
